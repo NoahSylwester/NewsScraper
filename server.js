6a3a40cf155acc81779850e36b0a8a9a68d2fef6
Noah Sylwester <nanoabram@comcast.net>
2019-11-19 23:50:59 -0800

    add article sorting by date

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ app.get("/scrape", function(req, res) {
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
   // Grab every document in the Articles collection
-  db.Article.find({})
+  db.Article.find({}).sort({ datetime: -1 })
     .then(function(dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
 
